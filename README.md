55110d22ede408bb81b06526efe5c20120cd52e3
Ahmed Mostafa (Buffa) <ahmedmostafametwaly@hotmail.com>
2021-06-24 13:53:01 +0200

    tidy up gh-pages branch to keep them with just index.html and images

--- README.md
+++
@@ -1,2 +0,0 @@
-# BellaBeat
-This project involves the analysis of proxy FitBit data in order to make data driven decisions for BellaBeat.
--- SQL files/Question1.sql
+++
@@ -1,7 +0,0 @@
-/*Question 1*/
-SELECT 
-	CAST(AVG(very_active_minutes) AS INT) AS "very",
-	CAST(AVG(moderately_active_minutes) AS INT) AS "moderate",
-	CAST(AVG(lightly_active_minutes) AS INT) AS "light", 
-	CAST(AVG(sedentary_active_minutes) AS INT) AS "inactive"
-FROM DailyActivity;
